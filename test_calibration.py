a19f8d4410a0115d13b14feefa67310aeae0dc98
sezan92 <sezan92@gmail.com>
2021-03-19 23:09:55 +0900

    test_calibration

--- test_calibration.py
+++ test_calibration.py
@@ -1,23 +1,27 @@
 import unittest
 
+import numpy
+
 import util
-from calibration import get_chessboard_corner
+from calibration import get_calibration_matrix, get_chessboard_corner
 
 
 class TestCalibration(unittest.TestCase):
     def test_get_chessboard_corner1(self):
         img = util.imread("camera_cal/calibration1.jpg")
-        object_point, corners = get_chessboard_corner(img, False)
+        corners = get_chessboard_corner(img, False)
 
-        assert object_point is None
         assert corners is None
 
     def test_get_chessboard_corner2(self):
         img = util.imread("camera_cal/calibration2.jpg")
-        object_point, corners = get_chessboard_corner(img, False)
+        corners = get_chessboard_corner(img, False)
 
-        assert object_point is not None
         assert corners is not None
+
+    def test_get_calibration_matrix(self):
+        cameraMatrix, distCoeffs = get_calibration_matrix("camera_cal")
+        assert isinstance(cameraMatrix, numpy.ndarray)
 
 
 if __name__ == "__main__":
