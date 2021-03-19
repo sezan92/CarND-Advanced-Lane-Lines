9d7937e2928f9a5b6158262b40e8828ae0a9dcfa
sezan92 <sezan92@gmail.com>
2021-03-19 22:07:00 +0900

    calibration modeule

---
+++ calibration.py
@@ -0,0 +1,18 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_calibration_matrix(image, plot):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    object_point = np.zeros((6 * 9, 3), np.float32)
+    object_point[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+    ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+
+    if ret:
+        image = cv2.drawChessboardCorners(image, (9, 6), corners, ret)
+        if plot:
+            plt.figure()
+            plt.imshow(image)
+
+    return object_point, corners
