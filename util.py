eeb021196d67ce717d24b9dec73aa058a1bb948d
sezan92 <sezan92@gmail.com>
2021-03-21 18:13:54 +0900

    util

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 
 
@@ -12,3 +14,23 @@ def imread(image_name):
     img = cv2.imread(image_name)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
+
+
+def load_img_from_dir(dir_name):
+    """
+    str -> list
+    Reads images from directory and returns the image arrays list
+
+    Parameters:
+        dir_name: str, directory name
+    Returns:
+        imgs: list, list of image arrays
+    """
+
+    img_paths = [
+        os.path.join(dir_name, img_path)
+        for img_path in os.listdir(dir_name)
+        if img_path.endswith("jpg")
+    ]
+
+    return [imread(img_path) for img_path in img_paths]
