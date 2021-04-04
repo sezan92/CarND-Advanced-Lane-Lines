65e9af01d94d6b9770abc79ca6dc378d8482efde
sezan92 <sezan92@gmail.com>
2021-04-04 21:08:51 +0900

    binary test

--- test_binary_transform.py
+++ test_binary_transform.py
@@ -15,6 +15,14 @@ class TestBinaryTransformer(unittest.TestCase):
         bw = self.transformer.tune(img)
         assert isinstance(bw, np.ndarray)
 
+    def test_tune_dir(self):
+        self.transformer = BinaryTransformer()
+        dir = "test_images"
+        imgs = util.load_img_from_dir(dir)
+        self.transformer.tune_dir(imgs)
+        assert isinstance(self.transformer.s_thresh, tuple)
+        assert isinstance(self.sx_thresh, tuple)
+
 
 if __name__ == "__main__":
     unittest.main()
