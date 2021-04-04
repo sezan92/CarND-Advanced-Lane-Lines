8d88e08fa856ffd3ab6d455a14602f9dc4460298
sezan92 <sezan92@gmail.com>
2021-04-04 21:08:09 +0900

    tune images

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,3 @@
-from functools import partial
-
 import cv2
 import numpy as np
 
@@ -69,9 +67,18 @@ class BinaryTransformer:
     def __nothing__(self, val):
         pass
 
-    def r2b(self, img, s1=150, s2=230, sx1=89, sx2=220):
-        s_thresh = (s1, s2)
-        sx_thresh = (sx1, sx2)
+    def r2b(self, img):
+        """
+        Converts RGB to Binary image
+        Arguments:
+            img: numpy array, input image
+            s1: int, s channel lowest threshold
+            s2: int, s channel highest threshold
+            sx1: int, sobel operator x axis lowest threshold
+            sx2: int, sobel operator x axis highest Threshold
+        Returns:
+            combined_binary: numpy array, binary image
+        """
         hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
         l_channel = hls[:, :, 1]
         s_channel = hls[:, :, 2]
@@ -80,17 +87,26 @@ class BinaryTransformer:
         scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
 
         sxbinary = np.zeros_like(scaled_sobel)
-        sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+        sxbinary[
+            (scaled_sobel >= self.sx_thresh[0]) & (scaled_sobel <= self.sx_thresh[1])
+        ] = 1
 
         s_binary = np.zeros_like(s_channel)
-        s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+        s_binary[(s_channel >= self.s_thresh[0]) & (s_channel <= self.s_thresh[1])] = 1
         combined_binary = np.zeros_like(sxbinary)
         combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
 
         return combined_binary
 
     def tune(self, img):
+        """
+        tunes a given image
+        parameters:
+            img: numpy.ndarray, the given image to tune
+        returns:
+            None
 
+        """
         cv2.namedWindow(self.tuning_title)
         cv2.createTrackbar("s1", self.tuning_title, 0, 255, self.__nothing__)
         cv2.createTrackbar("s2", self.tuning_title, 0, 255, self.__nothing__)
@@ -104,12 +120,47 @@ class BinaryTransformer:
             sx2 = cv2.getTrackbarPos("sx2", self.tuning_title)
             self.s_thresh = (s1, s2)
             self.sx_thresh = (sx1, sx2)
-            bw = self.r2b(img, s1, s2, sx1, sx2)
+            bw = self.r2b(img)
             cv2.imshow(self.tuning_title, bw * 255)
             k = cv2.waitKey(1) & 0xFF
             if k == 27:
                 cv2.destroyAllWindows()
                 break
 
+    def _get_mean_of_threshold(self, threshold_list):
+        """
+        Gets the mean of threshold list
+        Arguments:
+            threshold_list: list, list of tuples
+        Returns:
+            mean_value: tuple, mean of the tuples
+        """
+        sum1 = 0
+        sum2 = 0
+        for (t1, t2) in threshold_list:
+            sum1 = sum1 + t1
+            sum2 = sum2 + t2
+        mean_value = (sum1 / len(threshold_list), sum2 / len(threshold_list))
+
+        return mean_value
+
+    def tune_imgs(self, imgs):
+        """
+        Tunes a given directory and saves the average threshold values
+        Arguments:
+            imgs: list, list of images
+
+        """
+        s_values = []
+        sx_values = []
+        for img in imgs:
+            self.tune(img)
+            s_values.append(self.sx_thresh)
+            sx_values.append(self.sx_thresh)
+
+        self.s_thresh = self._get_mean_of_threshold(s_values)
+        self.sx_thresh = self._get_mean_of_threshold(sx_values)
+
     def transform(self, img):
-        pass
+
+        return self.r2b(img)
