0213c238ae72fa2171dd739e2b3d75ad25f798e1
sezan92 <sezan92@gmail.com>
2021-04-11 17:53:25 +0900

    test_binary_transformer

--- test_binary_transform.py
+++ test_binary_transform.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 
+import os
 import unittest
 
 import numpy as np
+import yaml
 
 import util
 from preprocess import BinaryTransformer
@@ -19,9 +21,15 @@ class TestBinaryTransformer(unittest.TestCase):
         self.transformer = BinaryTransformer()
         dir = "test_images"
         imgs = util.load_img_from_dir(dir)
-        self.transformer.tune_dir(imgs)
+        self.transformer.tune_imgs(imgs)
         assert isinstance(self.transformer.s_thresh, tuple)
-        assert isinstance(self.sx_thresh, tuple)
+        assert isinstance(self.transformer.sx_thresh, tuple)
+        assert os.path.exists("config.yaml")
+        with open("config.yaml", "r") as cfgh:
+            cfg = yaml.load(cfgh)
+        assert isinstance(cfg, dict)
+        assert "s_thresh" in cfg
+        assert "sx_thresh" in cfg
 
 
 if __name__ == "__main__":
