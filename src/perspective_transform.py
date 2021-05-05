55a9b37b90f54b72716ac830fcd8996ddb2c81d6
sezan92 <sezan92@gmail.com>
2021-05-06 01:37:26 +0900

    WIP perspective transform

---
+++ src/perspective_transform.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+import argparse
+from preprocess import PerspectiveTransformer
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--")
